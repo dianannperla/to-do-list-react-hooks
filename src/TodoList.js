f46e6f93d1623afff2d21a0cc918510b335137f3
dianannperla <waafirmelyza@hotmail.com>
2025-08-16 17:11:23 +0100

    feat: initial commit for src/TodoList.js

---
+++ src/TodoList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const TodoList = ({ todos, toggleComplete, deleteTodo }) => {
+  return (
+    <ul>
+      {todos.map((todo, index) => (
+        <li key={index} style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+          <input 
+            type="checkbox" 
+            checked={todo.completed} 
+            onChange={() => toggleComplete(index)} 
+          />
+          {todo.text}
+          <button onClick={() => deleteTodo(index)}>Xóa</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
